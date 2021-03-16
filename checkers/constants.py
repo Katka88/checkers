4c7179a04c1f7207bccb973a99e6c712d16b3c8d
Katka88 <57238773+Katka88@users.noreply.github.com>
2021-03-16 21:52:43 +0100

    Add files via upload

---
+++ checkers/constants.py
@@ -0,0 +1,12 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH/COLS
+
+#colours in rgb
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
